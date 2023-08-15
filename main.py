3ae814e878d1aa7cd73c4e551e4947bc933b0674
TheUserOne <theuserone@yandex.ru>
2023-08-15 22:08:24 +0700

    beginning

---
+++ main.py
@@ -0,0 +1,32 @@
+
+# print('enter how many kilometers a car can travel in a day')
+# n = float(input())
+# print('enter how many kilometers the car will travel')
+# m = float(input())
+# days = m / n
+# print(f"the trip will take {days} day(s)")
+
+
+
+# print('enter 1st class')
+# n = int(input())
+# print('enter 2nd class')
+# m = int(input())
+# print('enter 3rd class')
+# k = int(input())
+
+# desks = (n // 2) + (m // 2) + (k // 2)
+# desksrem = (n % 2) + (m % 2) + (k % 2)
+# total = desks + desksrem
+
+# print(f"the total desks {total}")
+
+
+
+
+# print('enter an integer')
+# n = int(input())
+# if n % 4 == 0 and n % 100 != 0 or n % 400 == 0 and n % 100 != 0:
+#     print('YES')
+# else:
+#     print('no')
